d3fd1dc91cc2a0b1d2156349f0d17875cdde3783
doofmars <jan-preuss@gmx.de>
2016-09-17 15:06:43 +0200

    Add inception easteregg

---
+++ js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function im(element, image) {
+    element.src = image;
+}
+
+
+
+function playNew() {
+    var id = 0;
+    var zone = document.getElementById('audioElements');
+    var tag = document.createElement('audio');
+    tag.setAttribute('preload', 'auto');
+    tag.setAttribute('autoplay', 'autoplay');
+    tag.setAttribute('src', '../resources/mp3_inception.mp3');
+    tag.setAttribute('id', 'audio' + id);
+    zone.appendChild(tag);
+    tag.play();
+    setTimeout('x=document.getElementById("audio" + id + "");x.parentNode.removeChild(x);', 3500);
+    id++;
+}
